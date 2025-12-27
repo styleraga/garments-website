b1011b1b5614e5431814a9a9d2699a05e3e6340b
styleraga <styleraga1@gmail.com>
2025-12-28 01:19:16 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const SHEET_ID = "1Z9WOaTbtrk54xzYZxjo_gdP2Xbf7AsWZuThs49vi53k";
+const SHEET_NAME = "Sheet1";
+
+let allProducts = [];
+
+fetch(`https://opensheet.elk.sh/${SHEET_ID}/${SHEET_NAME}`)
+  .then(res => res.json())
+  .then(data => {
+    allProducts = data.filter(p => p.Stock === "yes");
+    showCategory("all");
+  });
+
+function showCategory(category) {
+  const products = document.getElementById("products");
+  products.innerHTML = "";
+
+  allProducts.forEach(p => {
+    if (category === "all" || p.Category === category) {
+      products.innerHTML += `
+        <div class="product">
+          <img src="${p.ImageURL}">
+          <h4>${p.ProductName}</h4>
+          <p>৳${p.Price}</p>
+        </div>
+      `;
+    }
+  });
+}
+
+function showContact() {
+  document.getElementById("products").innerHTML = `
+    <div class="contact">
+      <h2>Contact Us</h2>
+      <p>Phone: 01XXXXXXXXX</p>
+      <p>WhatsApp Available</p>
+    </div>
+  `;
+}
